5711816631c4dee4acbaadf77c16f6ffaa0022af
pgrusell <gonzalezrusellp@gmail.com>
2025-09-01 11:19:23 +0200

    feat(gitignore): ignore data and result directories

---
+++ config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Load env variables
+export repopath=$(realpath .)
+
+# Create the data directory (if it does not exist yet)
+mkdir -p data
+
+# Create the results directory and its structure (if it does not exists yet)
+mkdir -p results
+mkdir -p results/{cal,sim,track,final}
