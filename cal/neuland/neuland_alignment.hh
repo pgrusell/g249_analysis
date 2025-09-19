6780c385a076ef84fdde8bf8eb8b1cdfe0807266
pgrusell <gonzalezrusellp@gmail.com>
2025-09-19 12:32:46 +0200

    feat(utils): new methods for setting the style of histograms
    feat(utils): added a function to set the style of the canvas
    
    chrore(utils): renamed histogramStyles as plotStyles
    
    feat(utils): added alpha for histograms
    
    feat(cal/neuland): new implementation (wrong?)
    
    feat(cal/neuland): ToF alignment done
    
    fix(utils): set OpenGL by default
    
    fix(cal/neuland): finished a broken line
    
    feat(cal/neuland): final implementation aligning with respect to velocity.
    
    feat(neuland/cal): class to analyze the alignment status
    
    fix(neuland/cal): minor fix
    
    feat(cal/neuland): new implementation aligning by speed
    
    feat(cal/neuland): final implementation of neuland aligment and analysis
    
    style(cal/neuland): removed useless files
    
    feat(cal/neuland): added new histograms to the analysis

---
+++ cal/neuland/neuland_alignment.hh
@@ -0,0 +1,298 @@
+// This class will:
+
+// 1) Calculate the offsets from son TH2D to align the tof
+// 2) Apply the offsets to show that it has worked
+
+// * If the TH2D with ToF vs Paddle does not exist it can be retrived
+//   from the corresponding rootfile with neuland hit data.
+
+#pragma once
+
+#include <fstream>
+
+#include "../../utils/plotStyles.h"
+#include "neuland_alignment_analysis.hh"
+
+class neulandAlignment
+{
+public:
+    neulandAlignment(TString hitsFile = "", std::vector<TString> dataFile = {}, int TofOrDt = 1)
+    {
+
+        // Absolute path of the file
+        TString histFilePath = static_cast<TString>(getenv("repopath")) + "/results/cal/" + hitsFile;
+
+        fResults = new TFile("resultsTest.root", "UPDATE");
+
+        // If the dataFile contains the histograms get it
+        if (hitsFile != "")
+        {
+
+            // auto *histDataFile = new TFile(histFilePath, "READ");
+
+            TString histName = TofOrDt ? "hUncorrectedToF" : "hUncorrecteddT";
+            auto *h = static_cast<TH2D *>(fResults->Get(histName));
+
+            // Clean the histogram
+            int threshold = 0;
+            TH2D *hTofVsPaddlesNotCleaned = (TH2D *)h->RebinY(1, "hTofVsPaddlesRebbined");
+            neulandAlignmentAnalysis::cleanHistogram(hTofVsPaddlesNotCleaned, hTofVsPaddles, "hTofVsPaddles", threshold);
+
+            hTofVsPaddles->SetDirectory(nullptr);
+            // delete histDataFile;
+        }
+        // Call the method to retrieve it from the data
+        else
+        {
+            buildHistogram(dataFile, false, TofOrDt);
+        }
+    }
+
+    ~neulandAlignment()
+    {
+        if (fResults)
+        {
+            fResults->Write();
+            fResults->Close();
+            delete fResults;
+            fResults = nullptr;
+        }
+    }
+
+    void buildHistogram(std::vector<TString> dataFileAbs, bool withOffsets = false, int TofOrDt = 1, int full = 0)
+    {
+
+        // Four options:
+        // - dT = time_hit - time_target - L_hit / c ( + time_offset)
+        // - ToF = [time_hit - time_target (+ time_offset)] / L_hit * L_0
+
+        TString absPath = static_cast<TString>(getenv("repopath")) + "/data/";
+
+        // Chain all files
+        TChain chain("evt");
+        for (const auto &f : dataFileAbs)
+        {
+            chain.Add(f);
+        }
+
+        // Reader
+        TTreeReader reader(&chain);
+        TTreeReaderArray<R3BNeulandHit> neuland(reader, "NeulandHits");
+        TTreeReaderArray<R3BLosHitData> los(reader, "LosHit");
+        int ievt = 0;
+
+        // If not offsets, set them to zero
+        if (!withOffsets)
+        {
+            fTofOffsets.clear();
+            fTofOffsets.resize(fNPaddles);
+        }
+
+        double binTofMin, binTofMax;
+        int nBinsTof;
+        TString histogramTitle;
+        TString units;
+
+        if (TofOrDt)
+        {
+            if (!full)
+            {
+                binTofMin = 0.;
+                binTofMax = 500;
+                nBinsTof = 11000;
+                histogramTitle = "hToF";
+                units = "ToF vs Paddle;Paddle;ToF (ns)";
+            }
+
+            else
+            {
+                binTofMin = 46.0;
+                binTofMax = 55.0;
+                nBinsTof = 200;
+                histogramTitle = "hToF_full";
+                units = "ToF vs Paddle;Paddle;ToF (ns)";
+            }
+        }
+        else
+        {
+            binTofMin = -50.;
+            binTofMax = 50.;
+            nBinsTof = 2200;
+            histogramTitle = "hdeltaTime";
+            units = "#Deltat vs Paddle;Paddle;#Deltat (ns)";
+        }
+
+        if (withOffsets)
+            histogramTitle += "Corrected";
+
+        histogramTitle += "VsPaddle";
+
+        auto *hTofVsPad = new TH2D(histogramTitle, units,
+                                   1300, -0.5, 1299.5,
+                                   nBinsTof, binTofMin, binTofMax);
+
+        // Calculate dT or ToF
+        const double Lref = 1557.0; // cm
+        const double c = 29.979;    // cm/ns
+        double timeTarget = 7.423887;
+
+        if (!withOffsets)
+            timeTarget = 0.;
+
+        auto computeTime = [=](double timeHit, double distHit, double timeOffset)
+        {
+            const double timeCorr = timeHit - timeTarget - timeOffset;
+
+            if (TofOrDt)
+                return timeCorr / distHit * Lref;
+            else
+                return timeCorr - distHit / c;
+        };
+
+        // Iterate over events
+        while (reader.Next())
+        {
+            if ((++ievt % 100000) == 0)
+            {
+                std::cout << 100.0 * ievt / chain.GetEntries() << " %\n";
+            }
+
+            // Only 1 hit in LOS
+            if (los.GetSize() != 1)
+                continue;
+
+            for (auto const &neu : neuland)
+            {
+
+                const double time = computeTime(neu.GetT(), neu.GetPosition().Mag(), fTofOffsets[neu.GetPaddle() - 1]);
+
+                if (!std::isnan(time) && neu.GetT() > 0)
+                {
+                    hTofVsPad->Fill(neu.GetPaddle() - 1, time);
+                }
+            }
+        }
+
+        withOffsets ? hTofVsPaddlesCorr = hTofVsPad : hTofVsPaddles = hTofVsPad;
+        TString titHist = withOffsets ? "hCorrected" : "hUncorrected";
+
+        if (TofOrDt)
+        {
+            if (full)
+                titHits += "_full";
+            titHist += "ToF";
+            else titHist += "dT";
+        }
+
+        auto *hToSave = hTofVsPad->Clone(titHist);
+        fResults->cd();
+        hToSave->Write("", TObject::kOverwrite);
+    }
+
+    // This method takes the corrected histogram from rootFile
+    void setCorrectedFromRoot(TString histoPath)
+    {
+        auto *histDataFile = new TFile(histoPath, "READ");
+        auto *h = static_cast<TH2D *>(histDataFile->Get("hTofVsPad"));
+        hTofVsPaddlesCorr = h;
+    }
+
+    // This method can be used to initialize the offsets
+    void calculateOffsets()
+    {
+
+        fTofOffsets.clear();
+
+        TH1D *profile;
+
+        // Iterate over all the bins and perform a Y projection
+        for (int i = 0; i < fNPaddles; i++)
+        {
+            profile = hTofVsPaddles->ProjectionY(Form("h_proj_%i", i + 1), i + 1, i + 1);
+            calculateOffsetFromProjection(profile);
+            delete profile;
+        }
+
+        std::ofstream out("offsets_v2.txt");
+
+        for (int i = 0; i < fNPaddles; i++)
+        {
+            out << i + 1 << " " << fTofOffsets[i] << " " << fFitPars[i][0] << " " << fFitPars[i][1] << std::endl;
+        }
+
+        out.close();
+    }
+
+    void setOffsetsFromTxt(std::string inputFile = "offsets_v1.txt")
+    {
+        std::ifstream input(inputFile);
+        std::string line;
+        fTofOffsets.resize(fNPaddles);
+
+        while (std::getline(input, line))
+        {
+            std::istringstream ss(line);
+            std::vector<double> fileVals;
+            double val;
+
+            while (ss >> val)
+            {
+                fileVals.push_back(val);
+            }
+
+            fTofOffsets[fileVals[0] - 1] = fileVals[1];
+        }
+    }
+
+     // Method to fit the particular profile
+    void calculateOffsetFromProjection(TH1D *profile)
+    {
+        if (profile->Integral() < 1)
+        {
+            fTofOffsets.push_back(0.);
+            fFitPars.push_back(std::vector<double>{0., 0.});
+            return;
+        }
+
+        int binMin = profile->FindBin(-9);
+        int binMax = profile->FindBin(-7.6);
+
+        int maxBin = binMin;
+        double maxContent = profile->GetBinContent(binMin);
+
+        for (int b = binMin + 1; b <= binMax; ++b)
+        {
+            double content = profile->GetBinContent(b);
+            if (content > maxContent)
+            {
+                maxContent = content;
+                maxBin = b;
+            }
+        }
+
+        double center = profile->GetBinCenter(maxBin);
+
+        // double center = profile->GetBinCenter(profile->GetMaximumBin());
+        double min = center - 0.5;
+        double max = center + 0.5;
+
+        auto f1 = new TF1("f_gauss", "gaus", 0, 0);
+        f1->SetRange(min, max);
+
+        profile->Fit(f1, "QR");
+        double offset = f1->GetParameter(1);
+
+        // profile->Draw();
+        fTofOffsets.push_back(offset);
+        fFitPars.push_back(std::vector<double>{f1->GetParameter(1), f1->GetParameter(2)});
+    }
+
+private:
+    const int fNPaddles = 1300;
+    TH2D *hTofVsPaddles = nullptr;
+    TH2D *hTofVsPaddlesCorr = nullptr;
+    TH2D *hOffsets = nullptr;
+    std::vector<double> fTofOffsets;
+    std::vector<std::vector<double>> fFitPars;
+    TFile *fResults = nullptr;
+};
---
+++ cal/neuland/neuland_alignment_analysis.hh
@@ -0,0 +1,323 @@
+#pragma once
+
+#include "../../utils/plotStyles.h"
+
+class neulandAlignmentAnalysis
+{
+public:
+    neulandAlignmentAnalysis(TString resultsFile = "")
+    {
+
+        TString resultsPath = static_cast<TString>(getenv("repopath")) + "/results/cal/";
+
+        TString histFilePath = resultsPath + resultsFile;
+        std::cout << histFilePath << std::endl;
+        auto *histDataFile = new TFile(histFilePath, "READ");
+
+        hUncorrecteddT = static_cast<TH2D *>(histDataFile->Get("hUncorrecteddT"));
+        hCorrecteddT = static_cast<TH2D *>(histDataFile->Get("hCorrecteddT"));
+
+        hUncorrectedToF = static_cast<TH2D *>(histDataFile->Get("hUncorrectedToF"));
+        hCorrectedToF = static_cast<TH2D *>(histDataFile->Get("hCorrectedToF"));
+
+        hToFFull = static_cast<TH2D *>(histDataFile->Get("hCorrected_fullToF"));
+
+        cleanHistogram(hUncorrecteddT, hUncorrectedCleaned, "hUncorrectedCleaned");
+        cleanHistogram(hCorrecteddT, hCorrectedCleaned, "hCorrectedCleaned");
+
+        // save ToF vs paddle histograms in pdf files
+        printCanvases(resultsPath);
+
+        // save the ToF projection over Y axis
+        totalProfile(resultsPath);
+
+        // plot profiles for uncorrected
+        checkAlignment(hUncorrecteddT, resultsPath + "single_prof_uncorr_dt", 8, -10, -6, "#Deltat [ns]", true);
+
+        // plot profiles for corrected
+        checkAlignment(hCorrecteddT, resultsPath + "single_prof_corr_dt.pdf", 8, -2, -12, "#Deltat [ns]", false);
+
+        // plot profiles for uncorrected
+        checkAlignment(hUncorrectedToF, resultsPath + "single_prof_uncorr_tof", 10, 46, 54, "ToF [ns]");
+
+        // plot profiles for corrected
+        checkAlignment(hCorrectedToF, resultsPath + "single_prof_corr_tof", 10, 46, 54, "ToF [ns]");
+
+        // Full ToF
+        printTofFull(resultsPath);
+
+        // Compare Full
+        compareTofFull(resultsPath);
+    }
+
+    static void cleanHistogram(TH2D *&h, TH2D *&hClean, TString name, int threshold = 30)
+    {
+        hClean = (TH2D *)h->Clone(name);
+
+        for (int ix = 1; ix <= hClean->GetNbinsX(); ix++)
+        {
+            for (int iy = 1; iy <= hClean->GetNbinsY(); iy++)
+            {
+                double content = hClean->GetBinContent(ix, iy);
+                if (content < threshold)
+                    hClean->SetBinContent(ix, iy, 0);
+            }
+        }
+    }
+
+    static void printCanvas(TH2D *&h, TString title, TString xTit, TString yTit, TString path, double ymin = 0, double ymax = 0, bool logy = false)
+    {
+
+        auto *c = new TCanvas("c", title, 800, 600);
+        setCanvasStyle(c, 0.15);
+        setHistogramStyle(h, xTit, yTit, 0, 0, ymin, ymax);
+        h->Draw("COLZ");
+        if (logy)
+            gPad->SetLogy();
+        c->SaveAs(path + title + ".pdf");
+        delete c;
+    }
+
+    void printCanvases(TString path)
+    {
+        setOpenGL();
+
+        printCanvas(hUncorrecteddT, "hUncorrecteddT", "Paddle #", "#Deltat [ns]", path, -10, -6);
+        printCanvas(hCorrecteddT, "hCorrecteddT", "Paddle #", "#Deltat [ns]", path, -2, 2);
+        printCanvas(hUncorrectedCleaned, "hUncorrectedCleaned", "Paddle #", "#Deltat [ns]", path, -10, -6);
+        printCanvas(hCorrectedCleaned, "hCorrectedCleaned", "Paddle #", "#Deltat [ns]", path, -2, 2);
+        printCanvas(hUncorrectedToF, "hUncorrectedToF", "Paddle #", "ToF [ns]", path);
+        printCanvas(hCorrectedToF, "hCorrectedToF", "Paddle #", "ToF [ns]", path);
+    }
+
+    void printTofFull(TString path)
+    {
+        auto *pr = hToFFull->ProjectionY("pr");
+        setHistogramStyle(pr, "ToF [ns]", "# counts", kCyan - 6, true);
+        auto *c = new TCanvas("c", "hTofFull", 800, 600);
+        setCanvasStyle(c);
+        gPad->SetLogy();
+        //  pr->Draw();
+        c->SaveAs(path + "hTofFull.pdf");
+        delete c;
+    }
+
+    void totalProfile(TString path)
+    {
+        auto *prCorrected = hCorrectedToF->ProjectionY("prCorr");
+        auto *prUncorrected = hUncorrectedToF->ProjectionY("prUncorr");
+
+        setHistogramStyle(prCorrected, "ToF [ns]", "# counts", kCyan - 6);
+        setHistogramStyle(prUncorrected, "ToF [ns]", "# counts", kOrange - 3);
+
+        auto *c5 = new TCanvas("c5", "hProfTotBoth", 800, 600);
+        setCanvasStyle(c5);
+
+        prCorrected->Draw();
+        prUncorrected->Draw("same");
+
+        auto *leg = new TLegend(0.15, 0.75, 0.25, 0.9);
+        leg->SetTextSize(0.04);
+        leg->SetBorderSize(0);
+        leg->SetFillStyle(0);
+        leg->AddEntry(prCorrected, "new params (FWHM = 0.6 ns)", "l");
+        leg->AddEntry(prUncorrected, "old params (FWHM = 0.8 ns)", "l");
+        leg->Draw();
+
+        c5->SaveAs(path + "hProfTot.pdf");
+
+        delete c5;
+    }
+
+    void checkAlignment(TH2D *&hInit, TString title, int nProfiles = 5, double xmin = -10, double xmax = -6, TString lab = "#Deltat [ns]", bool single = false)
+    {
+
+        TString titleOne = title + "One.pdf";
+        title += ".pdf";
+
+        TH2D *h = (TH2D *)hInit->RebinY(2, "h");
+
+        setOpenGL();
+        auto c6 = new TCanvas("c6", "", 800, 600);
+        setCanvasStyle(c6);
+
+        std::vector<std::pair<TH1D *, double>> profiles;
+        profiles.reserve(nProfiles);
+        const int nY = fNPaddles;
+
+        for (int i = 0; i < nProfiles; ++i)
+        {
+            const int num = 100 * i + 10;
+            TH1D *prof = h->ProjectionY(Form("h_prof_%d", i), num, num);
+            int binLow = prof->FindBin(xmin);
+            int binHigh = prof->FindBin(xmax);
+
+            double maxInRange = 0.0;
+            for (int b = binLow; b <= binHigh; ++b)
+            {
+                double content = prof->GetBinContent(b);
+                if (content > maxInRange)
+                    maxInRange = content;
+            }
+
+            profiles.emplace_back(prof, maxInRange);
+        }
+
+        std::sort(profiles.begin(), profiles.end(),
+                  [](const auto &a, const auto &b)
+                  { return a.second > b.second; });
+
+        auto colors = makeViridisColors(profiles.size());
+        auto legend = new TLegend(0.8 - 0.03, 0.7 - 0.03, 1. - 0.03, 1. - 0.03);
+
+        for (auto i = 0; i < colors.size(); i++)
+        {
+            int paddleNum = 100 * i + 10;
+            TString entry = Form("Paddle %d", paddleNum);
+            setHistogramStyle(profiles[i].first, lab, "# Counts", colors[i], xmin, xmax);
+
+            if (i == 0)
+                profiles[i].first->Draw();
+            else
+                profiles[i].first->Draw("SAME");
+
+            legend->AddEntry(profiles[i].first, entry, "l");
+        }
+
+        legend->SetLineWidth(2);
+
+        legend->Draw();
+        c6->SaveAs(title);
+
+        delete c6;
+        delete h;
+        delete legend;
+
+        if (single)
+        {
+            // Plot a fit over one of the profiles
+            auto c7 = new TCanvas("c7", "", 800, 600);
+            setCanvasStyle(c7);
+            int k = 2;
+            profiles[k].first->Draw();
+
+            int binMin = profiles[k].first->FindBin(xmin);
+            int binMax = profiles[k].first->FindBin(xmax);
+
+            int maxBin = binMin;
+            double maxContent = profiles[k].first->GetBinContent(binMin);
+
+            for (int b = binMin + 1; b <= binMax; ++b)
+            {
+                double content = profiles[k].first->GetBinContent(b);
+                if (content > maxContent)
+                {
+                    maxContent = content;
+                    maxBin = b;
+                }
+            }
+
+            double center = profiles[k].first->GetBinCenter(maxBin);
+
+            // double center = profile->GetBinCenter(profile->GetMaximumBin());
+            double min = center - 0.3;
+            double max = center + 0.3;
+
+            auto f1 = new TF1("f_gauss", "gaus", 0, 0);
+            f1->SetRange(min, max);
+
+            profiles[k].first->Fit(f1, "R");
+
+            auto leg = new TLegend(0.8, 0.85, 0.97, 0.95);
+
+            TString muStr = Form("#mu = %.2f ns", f1->GetParameter(1));
+            TString sigmaStr = Form("#sigma = %.2f ns", f1->GetParameter(2));
+
+            leg->AddEntry((TObject *)0, muStr, "");    // (TObject*)0 = texto sin símbolo
+            leg->AddEntry((TObject *)0, sigmaStr, ""); // segunda línea
+            leg->Draw();
+
+            c7->SaveAs(titleOne);
+
+            delete c7;
+        }
+    }
+
+    void compareTofFull(TString path, double m1 = -8.30623, double m2 = 0.0040615, double obj = 51.95, double ttar = 7.423887)
+    {
+        // Proyecciones (histos 1D)
+        hUncorrecteddT->GetYaxis()->SetRangeUser(-50, 50);
+        hCorrecteddT->GetYaxis()->SetRangeUser(-50, 50);
+        auto *prUncorr = (TH1D *)hUncorrecteddT->ProjectionY("prUncorr");
+        auto *prCorr = (TH1D *)hCorrecteddT->ProjectionY("prCorr");
+
+        // Si no pasas m1/m2, usa la media de las proyecciones
+        if (std::isnan(m1))
+            m1 = prUncorr->GetMean();
+        if (std::isnan(m2))
+            m2 = prCorr->GetMean();
+
+        const double shift1 = obj + (m1 + ttar) - m1;
+        const double shift2 = obj - m2;
+
+        const int nbx = prUncorr->GetNbinsX();
+        const double x1 = 0;
+        const double x2 = 100;
+
+        auto *hUncorrShifted = new TH1D("hUncorrShifted", "Uncorr shifted", nbx, x1, x2);
+        auto *hCorrShifted = new TH1D("hCorrShifted", "Corr shifted", nbx, x1, x2);
+        hUncorrShifted->Sumw2();
+        hCorrShifted->Sumw2();
+
+        hUncorrShifted->Draw();
+
+        for (int i = 1; i <= prUncorr->GetNbinsX(); ++i)
+        {
+            const double x = prUncorr->GetBinCenter(i);
+            const double y = prUncorr->GetBinContent(i);
+            if (y > 0)
+                hUncorrShifted->Fill(x + shift1, y);
+        }
+        for (int i = 1; i <= prCorr->GetNbinsX(); ++i)
+        {
+            const double x = prCorr->GetBinCenter(i);
+            const double y = prCorr->GetBinContent(i);
+            if (y > 0)
+                hCorrShifted->Fill(x + shift2, y);
+        }
+
+        // Estilo y dibujo
+        setOpenGL();
+        auto *c = new TCanvas("cf", "ToF compare", 800, 600);
+        setCanvasStyle(c);
+        setHistogramStyle(hCorrShifted, "ToF [ns]", "# counts", kCyan - 6);
+        setHistogramStyle(hUncorrShifted, "ToF [ns]", "# counts", kOrange - 3);
+
+        hCorrShifted->Draw("HIST");
+        hUncorrShifted->Draw("HIST SAME");
+
+        gPad->SetLogy();
+
+        auto *leg = new TLegend(0.8, 0.85, 0.95, 0.95);
+        leg->AddEntry(hCorrShifted, "New params", "l");
+        leg->AddEntry(hUncorrShifted, "Old params", "l");
+        leg->Draw();
+
+        c->SaveAs(path + "ToFCorrectedVsUncorrected.pdf");
+        // delete c;
+    }
+
+private:
+    const int fNPaddles = 1300;
+
+    TH2D *hUncorrecteddT = nullptr;
+    TH2D *hCorrecteddT = nullptr;
+
+    TH2D *hToFFull = nullptr;
+
+    TH2D *hUncorrectedToF = nullptr;
+    TH2D *hCorrectedToF = nullptr;
+
+    TH2D *hUncorrectedCleaned = nullptr;
+    TH2D *hCorrectedCleaned = nullptr;
+};
