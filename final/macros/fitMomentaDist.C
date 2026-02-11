04f4a65b15fd3c8e0f656e853f275ff53c0053e2
pgrusell <gonzalezrusellp@gmail.com>
2026-02-11 14:27:31 +0100

    Modify macro to read the new theoretical momentum distributions (which are also wrong...)

--- final/macros/fitMomentaDist.C
+++ final/macros/fitMomentaDist.C
@@ -123,85 +123,37 @@ void fitMomentaDist()
 {
     // In files
     std::vector<std::string> inFilesTheo = {
-        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-2s12-Sp-35.392.txt",
-        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-2s12-Sp-33.000.txt",
-        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-2s12-Sp-30.930.txt",
-
-        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-2s12-Sp-29.850.txt",
-        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-1p32-Sp-35.392.txt",
-        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-1p32-Sp-33.000.txt",
-
-        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-1p12-Sp-35.392.txt",
-        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-1p12-Sp-33.000.txt",
-        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-1p12-Sp-30.930.txt",
-
-        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-1d52-Sp-35.392.txt",
-        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-1d52-Sp-33.000.txt",
-        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-1d52-Sp-30.930.txt",
-
-        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-1d52-Sp-29.850.txt",
-        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-1d52-Sp-25.509.txt",
+        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-1d32-Sp-18.469.txt",
+        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-2s12-Sp-12.403.txt",
+        "/nucl_lustre/pablogrusell/g249/g249_analysis/theory/25F(p,2p)-1d52-Sp-15.969.txt",
     };
 
     std::string inFileExp = "/nucl_lustre/pablogrusell/g249/g249_analysis/results/final/24O_analyzed_test.root";
 
     // Read the histograms from the .txt file
-    auto momdis_2s12_35 = getMomentaDistFromtxt(inFilesTheo[0], "2s12_35");
-    auto momdis_2s12_33 = getMomentaDistFromtxt(inFilesTheo[1], "2s12_33");
-    auto momdis_2s12_25 = getMomentaDistFromtxt(inFilesTheo[2], "2s12_25");
-
-    auto momdis_2s12_29 = getMomentaDistFromtxt(inFilesTheo[3], "2s12_29");
-
-    auto momdis_1p32_35 = getMomentaDistFromtxt(inFilesTheo[4], "1p32_35");
-    auto momdis_1p32_33 = getMomentaDistFromtxt(inFilesTheo[5], "1p32_33");
-
-    auto momdis_1p12_35 = getMomentaDistFromtxt(inFilesTheo[6], "1p12_35");
-    auto momdis_1p12_33 = getMomentaDistFromtxt(inFilesTheo[7], "1p12_33");
-    auto momdis_1p12_30 = getMomentaDistFromtxt(inFilesTheo[8], "1p12_30");
-
-    auto momdis_1d52_35 = getMomentaDistFromtxt(inFilesTheo[9], "1d52_35");
-    auto momdis_1d52_33 = getMomentaDistFromtxt(inFilesTheo[10], "1d52_33");
-    auto momdis_1d52_30 = getMomentaDistFromtxt(inFilesTheo[11], "1d52_30");
-
-    auto momdis_1d52_29 = getMomentaDistFromtxt(inFilesTheo[12], "1d52_29");
-    auto momdis_1d52_25 = getMomentaDistFromtxt(inFilesTheo[13], "1d52_25");
+    auto momdis_1d32_18 = getMomentaDistFromtxt(inFilesTheo[0], "1d32_18");
+    auto momdis_2s12_12 = getMomentaDistFromtxt(inFilesTheo[1], "2s12_12");
+    auto momdis_1d52_15 = getMomentaDistFromtxt(inFilesTheo[2], "1d52_15");
 
     // Read the experimental histogram from the .root file
     auto momdis_exp = getMomentaDistFromRoot(inFileExp);
 
     // Resample the theoretical histogram to the bins of the experimental one
-    auto momdis_2s12_35_resampled = reampleMomDis(momdis_2s12_35, momdis_exp, "2s12_35");
-    auto momdis_2s12_33_resampled = reampleMomDis(momdis_2s12_33, momdis_exp, "2s12_33");
-    auto momdis_2s12_29_resampled = reampleMomDis(momdis_2s12_29, momdis_exp, "2s12_29");
-    auto momdis_2s12_25_resampled = reampleMomDis(momdis_2s12_25, momdis_exp, "2s12_25");
-
-    auto momdis_1p32_35_resampled = reampleMomDis(momdis_1p32_35, momdis_exp, "1p32_35");
-    auto momdis_1p32_33_resampled = reampleMomDis(momdis_1p32_33, momdis_exp, "1p32_33");
-
-    auto momdis_1p12_35_resampled = reampleMomDis(momdis_1p12_35, momdis_exp, "1p12_35");
-    auto momdis_1p12_33_resampled = reampleMomDis(momdis_1p12_33, momdis_exp, "1p12_33");
-    auto momdis_1p12_30_resampled = reampleMomDis(momdis_1p12_30, momdis_exp, "1p12_30");
-
-    auto momdis_1d52_35_resampled = reampleMomDis(momdis_1d52_35, momdis_exp, "1d52_35");
-    auto momdis_1d52_33_resampled = reampleMomDis(momdis_1d52_33, momdis_exp, "1d52_33");
-    auto momdis_1d52_30_resampled = reampleMomDis(momdis_1d52_30, momdis_exp, "1d52_30");
-    auto momdis_1d52_29_resampled = reampleMomDis(momdis_1d52_29, momdis_exp, "1d52_29");
-    auto momdis_1d52_25_resampled = reampleMomDis(momdis_1d52_25, momdis_exp, "1d52_25");
+    auto momdis_1d32_18_resampled = reampleMomDis(momdis_1d32_18, momdis_exp, "1d32_18");
+    auto momdis_2s12_12_resampled = reampleMomDis(momdis_2s12_12, momdis_exp, "2s12_12");
+    auto momdis_1d52_15_resampled = reampleMomDis(momdis_1d52_15, momdis_exp, "1d52_15");
 
     // Fit
-    TObjArray *mc = new TObjArray(2);
-    mc->Add(momdis_1d52_29_resampled.Qy);
-    mc->Add(momdis_1d52_25_resampled.Qy);
-
-    // mc->Add(momdis_1p12_30_resampled.Qy);
-    // mc->Add(momdis_1p12_30_resampled.Qy);
+    TObjArray *mc = new TObjArray(3);
+    mc->Add(momdis_1d32_18_resampled.Qy);
+    mc->Add(momdis_1d32_18_resampled.Qy);
+    mc->Add(momdis_1d52_15_resampled.Qy);
 
     TFractionFitter *fit = new TFractionFitter(momdis_exp, mc);
 
     fit->Constrain(0, 0.0, 1.0);
     fit->Constrain(1, 0.0, 1.0);
-    // fit->Constrain(2, 0.0, 1.0);
-    // fit->Constrain(3, 0.0, 1.0);
+    fit->Constrain(2, 0.0, 1.0);
 
     Int_t status = fit->Fit();
     std::cout << "fit status: " << status << std::endl;
