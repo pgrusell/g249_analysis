463b6f3e998aa6bd7823cf1337d41b6427a102a4
pgrusell <gonzalezrusellp@gmail.com>
2026-04-16 14:00:35 +0200

    macro to analyze JT calculations.

--- final/macros/momentum_distributions/runDataAnalysis.C
+++ final/macros/momentum_distributions/runDataAnalysis.C
@@ -3,10 +3,17 @@
 void runDataAnalysis(TString mode = "ana")
 {
 
-    if (mode == "ana")
+    if (mode == "ana23")
     {
         auto *da = new DataAnalysis("/nucl_lustre/pablogrusell/g249/g249_analysis/results/final/data_23O_test.root", "23O_analyzed_test.root", true);
         da->setOffsetsFromTxt("23O1n.txt");
+        da->getData();
+    }
+
+    if (mode == "ana24")
+    {
+        auto *da = new DataAnalysis("/nucl_lustre/pablogrusell/g249/g249_analysis/results/final/data_24O_test.root", "24O_analyzed_test.root", false);
+        // da->setOffsetsFromTxt("23O1n.txt");
         da->getData();
     }
 
